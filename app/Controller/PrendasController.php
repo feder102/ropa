8d7aa82efda842e1a0066ac4cf04c82705633f90
Federico Castiglione <feder102@gmail.com>
2017-05-06 17:31:56 -0300

    Corregido

--- app/Controller/PrendasController.php
+++ app/Controller/PrendasController.php
@@ -63,22 +63,25 @@ class PrendasController extends AppController {
 			//PRIMERO DEBEMOS GUARDAR LA PRENDA Y AHI VA A FUNCIONAR EL $THIS->PRENDA->ID
 
 			if ($this->Prenda->save($Prenda)) {
-				foreach ($fede['itemPrenda'] as $ip) {
-					$aux = array();
-					// pr($ip);
-					if($ip['id_color']){
-						$aux['id_prenda'] = $this->Prenda->id;
-						$aux['id_color'] = $ip['id_color'];
-						$aux['id_talle'] = $ip['id_talle'];
-						$aux['stock'] = $ip['stock'];
-						// $aux['created'] = date("dd-mm-YYYY hh:mm:ss", strtotime($ip['created']));
-						array_push($itemPrenda, $aux);
+				if(isset($fede['itemPrenda'])){
+					foreach ($fede['itemPrenda'] as $ip) {
+						$aux = array();
+						// pr($ip);
+						if($ip['id_color']){
+							$aux['id_prenda'] = $this->Prenda->id;
+							$aux['id_color'] = $ip['id_color'];
+							$aux['id_talle'] = $ip['id_talle'];
+							$aux['stock'] = $ip['stock'];
+							// $aux['created'] = date("dd-mm-YYYY hh:mm:ss", strtotime($ip['created']));
+							array_push($itemPrenda, $aux);
+						}
 					}
+					$Prenda['itemPrenda'] = $itemPrenda;
+					pr($fede['itemPrenda']);
+					$this->ItemPrenda->saveAll($itemPrenda);
+	            				$this->Session->setFlash(__('El/La prenda ha sido guardado/a.'), 'default', array('class' => 'alert alert-success'));
 				}
-				$Prenda['itemPrenda'] = $itemPrenda;
-				pr($fede['itemPrenda']);
-				$this->ItemPrenda->saveAll($itemPrenda);
-            				$this->Session->setFlash(__('El/La prenda ha sido guardado/a.'), 'default', array('class' => 'alert alert-success'));
+
 				// return $this->redirect(array('action' => 'index'));
 			} else {
 				$this->Session->setFlash(__('El/la prenda no se pudo guardar. Por favor, intente nuevamente.'), 'default', array('class' => 'alert alert-danger'));
